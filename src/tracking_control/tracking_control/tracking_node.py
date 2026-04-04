1a5c52c2f458c5b1cc0db3bdadd93ff77d5327f4
alexisvallier <163680885+alexisvallier@users.noreply.github.com>
2026-04-04 13:50:16 -0400

    Fix calculation of dx and dy in tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -293,8 +293,8 @@ class TrackingNode(Node):
             #### New code ####
             rx = self.robot_world_x
             ry = self.robot_world_y
-            dx = self.start_pose[0] - rx
-            dy = self.start_pose[1] - ry
+            dx = rx - self.start_pose[0]
+            dy = ry - self.start_pose[1]
 
             ##New Code - 04/03 ##
             #self.get_logger().info(rx)
