d7cddd73660c44b593128bc4a37be1d9b9eb12f0
Alexis Vallier <vallia2@rpi.edu>
2026-04-21 15:48:07 -0400

    Adjust weight of attractive potential

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -81,7 +81,7 @@ class TrackingNode(Node):
         self.attract_x = 0.0
         self.attract_y = 0.0
 
-        self.k_a = 0.5
+        self.k_a = 1.0
         self.k_r = 0.005
 
         # State for control
